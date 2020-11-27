18a4cd689fd69ff7aadcf043bc316538c1bb4f2a
RigvedBhavsar <rigvedbhavsar@gmail.com>
2020-11-27 17:35:07 +0530

    Project Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { ListproductComponent } from './listproduct/listproduct.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddproductComponent,
+    ListproductComponent
   ],
   imports: [
     BrowserModule,
