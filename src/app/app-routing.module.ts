eb0e4c37021c4ffe11313fc7e43e6e8b911e8570
RigvedBhavsar <rigvedbhavsar@gmail.com>
2020-11-27 21:21:39 +0530

    Added Some front End

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { HomeComponent } from './home/home.component';
+import { ListproductComponent } from './listproduct/listproduct.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'add' , component:AddproductComponent},
+    {path:'list', component:ListproductComponent},
+    {path:'',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddproductComponent } from './addproduct/addproduct.component';
 import { ListproductComponent } from './listproduct/listproduct.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatChipsModule} from '@angular/material/chips';
 @NgModule({
   declarations: [
     AppComponent,
     AddproductComponent,
-    ListproductComponent
+    ListproductComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
